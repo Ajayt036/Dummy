a4997765a77a1a922ef0b41174d87cd65d0c2491
Ajay Thakur <ajayt036@gmail.com>
2022-01-08 00:05:06 -0800

    Check

--- sp.py
+++ sp.py
@@ -1,5 +1,14 @@
-#This is my first file.Lets See how ot works
+'''
+Q1. A three digit number is said to be an “Armstrong number” if the sum of the third power of its individual digits is equal to the number itself. Example: 371 is an Armstrong number as 371 = 3 3 + 7 3 + 1 3 407 is an Armstrong number as 407 = 4 3 + 0 3 + 7 3 Write a pseudo-code to check whether a given three digit number is an Armstrong number.
+''' 
 
-#Add two Digits 5 and 6
-print(5+6)
+x = int(input("Enter your number"))
+while x != abs(x):
+    x = int(input("Only +ve number are acceptable, please eneter again"))
+y = str(x)
+y = map(int,y)           #This is a method of mapping each item of a string into integer.
+y = list(y)
+
+print("your number is Armstrong") if x == sum([number**3 for number in y]) else print("No its not Armstrong")
+print("minumun digit in your three digit number was: ", min(y))
 
