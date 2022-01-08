076b0084023322b23dcf0946894d7e012d2940d0
Ajay Thakur <ajayt036@gmail.com>
2022-01-08 00:07:14 -0800

    Check

--- sp.py
+++ sp.py
@@ -1,14 +1,15 @@
 '''
-Q1. A three digit number is said to be an “Armstrong number” if the sum of the third power of its individual digits is equal to the number itself. Example: 371 is an Armstrong number as 371 = 3 3 + 7 3 + 1 3 407 is an Armstrong number as 407 = 4 3 + 0 3 + 7 3 Write a pseudo-code to check whether a given three digit number is an Armstrong number.
+Q1. Remove repeating chacaters form following string x = "Python is the best programming language"1. A three digit number is said to be an “Armstrong number” if the sum of the third power of its individual digits is equal to the number itself. Example: 371 is an Armstrong number as 371 = 3 3 + 7 3 + 1 3 407 is an Armstrong number as 407 = 4 3 + 0 3 + 7 3 Write a pseudo-code to check whether a given three digit number is an Armstrong number.
 ''' 
 
-x = int(input("Enter your number"))
-while x != abs(x):
-    x = int(input("Only +ve number are acceptable, please eneter again"))
-y = str(x)
-y = map(int,y)           #This is a method of mapping each item of a string into integer.
-y = list(y)
-
-print("your number is Armstrong") if x == sum([number**3 for number in y]) else print("No its not Armstrong")
-print("minumun digit in your three digit number was: ", min(y))
-
+x = "Python is the best programming language"
+x = x.lower()
+y = list(x)
+z = []
+for i in y:
+    if y.count(i) > 1:
+        y.remove(i)
+for i in y:
+    if y.count(i) > 1:
+        y.remove(i)
+print(y)
